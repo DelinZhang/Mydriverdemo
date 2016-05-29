55d55a2936949d101b3ce9a7e6fe3381c22530a0
DelinZhang <Delin@appledemacbook-pro.local>
2016-05-29 10:07:33 +0800

    Initial Commit

---
+++ Mydriverdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mydriverdemo
+//
+//  Created by Delin on 16/5/29.
+//  Copyright © 2016年 Delin-Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mydriverdemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mydriverdemo
+//
+//  Created by Delin on 16/5/29.
+//  Copyright © 2016年 Delin-Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
